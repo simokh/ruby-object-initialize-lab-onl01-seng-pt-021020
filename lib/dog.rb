92cb6da0aecee4efc064b766f84dd4a5578cdce8
Simo Khouili <m.simokhouili@gmail.com>
2020-02-28 04:00:25 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,15 +1,18 @@
 class Dog
 
   def initialize(name, breed)
-    @name= name, breed
+    @name= name
+    @breed= breed
   end
 
   def name
-    @name = name
+    @name= name
+    @breed= breed
   end
 
   def name
     name
+    breed 
   end
 
 end
