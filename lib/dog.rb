b31dc3368c6a3a39699326293eaf30909853296c
Simo Khouili <m.simokhouili@gmail.com>
2020-02-28 04:06:28 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,7 +2,7 @@ class Dog
 
   def initialize(name, breed)
     @name= name
-    @breed= breed
+    @breed= "Mutt"
   end
 
   def name
@@ -10,7 +10,7 @@ class Dog
   end
 
   def breed
-    @breed= breed 
+    @breed= breed
   end
 
   def breed
