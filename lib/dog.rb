9e25b741c1c313b161cc08262741ebfa680c6e58
simokh <m.simokhouili@gmail.com>
2020-04-29 00:44:04 -0400

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,24 +1,8 @@
 class Dog
 
   def initialize(name, breed= "Mutt")
-    @name= name
-    @breed= breed
-  end
+    @name = name 
+    @breed = breed 
+  end 
 
-  def name
-    @name= name
-  end
-
-  def breed
-    @breed= breed
-  end
-
-  def name
-    name
-  end
-
-  def breed
-    breed
-  end
-
-end
+end 
--- lib/person.rb
+++ lib/person.rb
@@ -1,14 +1,7 @@
-class Person
+class Person 
 
   def initialize(name)
-    @name= name
-  end
-
-  def name
-    @name= name
-  end
-
-  def name
-    name
+    @name = name 
   end 
-end
+
+end 
